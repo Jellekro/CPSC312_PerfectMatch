bee7f4dd7ca52e5580daa1d824a72c4543cd3a5a
Andy Worth <k8m1b@ugrad.cs.ubc.ca>
2021-04-11 19:10:02 -0700

    fixed profile representation and corrected menPrefs

--- project2.pl
+++ project2.pl
@@ -13,60 +13,65 @@
 
 % Iteration Over List of Men & List of Women to Create Scores
 
-menPrefs(profile(_ ,male, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign), Prefs) :-
-findall(F, female(F), Females), 
-ourmap(profile(_ ,male, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign),transform, Females, Prefs).
+menPrefs(MaleID, Prefs) :-
+	findall(ID, female(ID), Females), 
+	scoreFemales(transform, MaleID, Females, Prefs).
 
-ourmap(_, _,[],[]).
-ourmap(M,P,[A|As],[B|Bs]) :- call(P,M,A,B),ourmap(M,P,As,Bs).
+scoreFemales(_, _, [], []).
+scoreFemales(P, M, [A|As], [B|Bs]) :-
+	call(P, M, A, B),
+	scoreFemales(P, M, As, Bs).
 
-transform(profile(_ ,male, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign), 
-	profile(Name2 ,female, Age2, (_,_), _, (Lat2, Lon2), Bio2, Star_Sign2), Y) :-
-Y is (Name2, score(profile(_ ,male, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign), 
-	profile(Name2 ,female, Age2, (_,_), _, (Lat2, Lon2), Bio2, Star_Sign2))).
+transform(MaleID, FemaleID, FemaleScore) :-
+	score(MaleID, FemaleID, Score),
+	FemaleScore = (FemaleID, Score).
 
-% female(F) is true if person F is a female
-female(profile(_, female, _, (_,_), _, (_,_), _, _)).
+% true if ID belongs to a male profile
+male(ID) :- profile(ID, _, male, _, (_,_), _, (_,_), _, _).
 
-% male(M) is true if person M is a male
-male(profile(_, male, _, (_,_), _, (_,_), _, _)).
+% true if ID belongs to a female profile
+female(ID) :- profile(ID, _, female, _, (_,_), _, (_,_), _, _).
 
 
-% Compatibility Scoring Functions (Soft + Hard Constraints)
 
-% score is true if Result is softscore or zero.
-score(profile(_, Gender, Age, (MinAge,MaxAge), City, (Lat, Lon), Bio, Star_Sign), profile(_, Gender2, Age2, (MinAge2,MaxAge2), City2, (Lat2, Lon2), Bio2, Star_Sign2), Result) :-
-(hardscore(profile(_, Gender, Age, (MinAge,MaxAge), City, (_,_), _, _), 
-	profile(_, Gender2, Age2, (MinAge2,MaxAge2), City2, (_,_), _, _)) -> 
-	softscore(profile(_, _, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign), 
-		profile(_, _, Age2, (_,_), _, (Lat2, Lon2), Bio2, Star_Sign2), Result) ; Result is 999999). 
-% FIX MAX INT ABOVE TODO
-% hardscore is true if the two profiles meet the hard constraints
-hardscore(profile(_, male, Age, (MinAge,MaxAge), City, (_,_), _, _), 
-	profile(_, female, Age2, (MinAge2,MaxAge2), City, (_,_), _, _)) :-
-MinAge2=<Age, Age=<MaxAge2, MinAge=<Age2, Age2=<MaxAge.
+% Compatibility Scoring Functions (Soft & Hard Constraints)
 
-% softscore is true if all scores are generated and added to result
-softscore(profile(_, _, Age, (_,_), _, (Lat, Lon), Bio, Star_Sign), profile(_, _, Age2, (_,_), _, (Lat2, Lon2), Bio2, Star_Sign2), Result) :-
-scoreAge(Age,Age2, Ares), 
-scoreDist(Lat,Lon,Lat2,Lon2, Bres), 
-scoreBio(Bio, Bio2, Cres), 
-scoreStar(Star_Sign, Star_Sign2, Dres), 
-Result is Ares + Bres + Cres + Dres.
+% true if Result is the softscore Result or 999999999 if the hard constraints failed (a large number to fit our minimizer goal).
+score(MaleID, FemaleID, Result) :-
+	hardscore(MaleID, FemaleID) ->
+		softscore(MaleID, FemaleID, Result);
+		Result is 999999999.
 
-% scoreAge is true if Ares is equal to the difference in Age and Age2.
-scoreAge(Age,Age2,Ares) :-
-	(Age > Age2 -> Ares is Age - Age2 ; Ares is Age2 - Age).
+% true if the two profiles meet the hard constraints (same city, opposite gender, age within partner range)
+hardscore(MaleID, FemaleID) :-
+	profile(MaleID, _, male, Age1, (MinAge1,MaxAge1), City, (_,_), _, _),
+	profile(FemaleID, _, female, Age2, (MinAge2,MaxAge2), City, (_,_), _, _),
+	MinAge2=<Age1, Age1=<MaxAge2,
+	MinAge1=<Age2, Age2=<MaxAge1.
 
-% scoreDist is true if Bres is equal to the difference in eucleandian distance between (Lat,Lon) and (Lat2,Lon2).
-scoreDist(Lat,Lon,Lat2,Lon2, Bres) :-
-	A is Lat - Lat2, B is A*A, C is Lon - Lon2, D is C*C, Bres is sqrt(B + D).
+% true if Result is the sum of the difference soft score aspect results
+softscore(MaleID, FemaleID, Result) :-
+	profile(MaleID, _, _, Age1, (_,_), _, (Lat1, Lon1), Bio1, Star_Sign1),
+	profile(FemaleID, _, _, Age2, (_,_), _, (Lat2, Lon2), Bio2, Star_Sign2),
+	scoreAge(Age1, Age2, ResultAge), 
+	scoreDist((Lat1,Lon1), (Lat2,Lon2), ResultDist), 
+	scoreBio(Bio1, Bio2, ResultBio), 
+	scoreStar(Star_Sign1, Star_Sign2, ResultStar), 
+	Result is ResultAge + ResultDist + ResultBio + ResultStar.
 
-% scoreBio is true if Cres is equal to the shared number of keywords * -1.
-% scoreBio(Bio, Bio2, Cres) : - TODO
+% true if ResultAge is the difference between Age1 and Age2
+scoreAge(Age1, Age2, ResultAge) :-
+	ResultAge is abs(Age1 - Age2).
+
+% true if ResultDist is the euclidean distance between (Lat1,Lon1) and (Lat2,Lon2)
+scoreDist((Lat1,Lon1), (Lat2,Lon2), ResultDist) :-
+	ResultDist is sqrt((Lat1-Lat2)**2 + (Lon1-Lon2)**2).
+
+% true if ResultBio is equal to the shared number of keywords between Bio1 and Bio2, multiplied by -1 (to fit our minimizer goal)
+% scoreBio(Bio1, Bio2, ResultBio) : - TODO
 scoreBio(_,_,0).
 
-% scoreStar is true if Dres is -1 for compatible, 0 otherwise
+% true if ResultStar is -1 for compatible (to fit our minimizer goal), and 0 otherwise
 %scoreStar(,,-1).
 %scoreStar(,,-1).
 %scoreStar(,,-1).
@@ -91,11 +96,8 @@ scoreBio(_,_,0).
 %scoreStar(,,-1).
 scoreStar(_,_,0).
 
-% The Database of Facts to be queried
-
-% profile(Name, Gender, Age, (MinAge,MaxAge), City, (Lat, Lon), Bio, Star_Sign)
-profile(george, male, 21, (18,25), vancouver, (34,25), "I am George and I like tacos", pisces).
-profile(barney, male, 35, (29,39), new_york, (900,342), "I am Barney and I like the beach", cancer). 
-profile(ashley, female, 24, (20,27), vancouver, (38, 27), "I am Ashley and I like running", taurus).
-profile(jessica, female, 40, (35,42), new_york, (900, 342), "I am Jessica and I like tacos", aquarius).
-
+% The Database of Profiles of format: profile(ID, Name, Gender, Age, (MinAge,MaxAge), City, (Lat, Lon), Bio, Star_Sign)
+profile(p1, george, male, 21, (18,25), vancouver, (34,25), "I am George and I like tacos", pisces).
+profile(p2, barney, male, 35, (29,39), new_york, (900,342), "I am Barney and I like the beach", cancer). 
+profile(p3, ashley, female, 24, (20,27), vancouver, (38, 27), "I am Ashley and I like running", taurus).
+profile(p4, jessica, female, 40, (35,42), new_york, (900, 342), "I am Jessica and I like tacos", aquarius).
